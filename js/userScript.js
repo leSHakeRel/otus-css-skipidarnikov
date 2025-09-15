f1134a693e0f99b88654299f37a9d91983d221f1
AlexS <skipidarnikov@gmail.com>
2025-09-15 20:08:28 +0300

    Добавлена обработка закрытия

--- js/userScript.js
+++ js/userScript.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const closeBtn = document.querySelector('#closeFormButton');
+    const closeBtn = document.querySelector('.closeFormButton');
     closeBtn.addEventListener('click', () => {
         const formElement = document.querySelector('.form');
         formElement.classList.remove('active');
+    });
+
+    const openBtn = document.querySelector('.openFormButton');
+    closeBtn.addEventListener('click', () => {
+        const formElement = document.querySelector('.form');
+        formElement.classList.add('active');
     });
 });

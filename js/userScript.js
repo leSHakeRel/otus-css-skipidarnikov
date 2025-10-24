84627fab40adaf1d88fc3380fd13f5c8bb0e2b77
AlexS <skipidarnikov@gmail.com>
2025-10-24 19:34:05 +0300

    fixed popup

--- js/userScript.js
+++ js/userScript.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const closeBtn = document.querySelector('.callback__reject');
-    const openBtn = document.querySelector('.contactsButton');
-    const acceptBtn = document.querySelector('.callback__accept');
+  const closeBtn = document.querySelector(".callback__reject");
+  const openBtn = document.querySelector(".contactsButton");
+  const acceptBtn = document.querySelector(".callback__accept");
 
-    let funToggle = () => {
-        const formElement = document.querySelector('.modal');
-        formElement.classList.toggle('hidden');
-        document.querySelector('body').classList.toggle('disable-scroll');
-    };
+  let funToggle = () => {
+    const element = document.getElementById("modal");
 
-    closeBtn.addEventListener('click', funToggle);
-    openBtn.addEventListener('click', funToggle);
-    acceptBtn.addEventListener('submit', funToggle);
+    if (element.getAttribute("visible") == "visible")
+      element.setAttribute("visible", "hidden");
+    else element.setAttribute("visible", "visible");
+
+    document.querySelector("body").classList.toggle("disable-scroll");
+  };
+
+  closeBtn.addEventListener("click", funToggle);
+  openBtn.addEventListener("click", funToggle);
+  acceptBtn.addEventListener("submit", funToggle);
 });
